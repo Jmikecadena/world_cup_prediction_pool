9289484b6bfa3d84ffbfa082c7a1ed0bbb55de72
Mike <jcadena0731@gmail.com>
2026-04-29 20:20:59 -0500

    redirection bug on mobiles

--- js/login.js
+++ js/login.js
@@ -1,14 +1,11 @@
 import { auth, provider, db } from "./firebase.js";
-import { signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { signInWithPopup, signInWithRedirect, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-getRedirectResult(auth).then(async (result) => {
-    if (!result) {
-        return;
-    }
-    const user = result.user;
+onAuthStateChanged(auth, async (user) => {
+    if (!user) return;
     const userRef = doc(db, "predicciones", user.uid);
     const userSnap = await getDoc(userRef);
     if (!userSnap.exists()) {
@@ -16,10 +13,7 @@ getRedirectResult(auth).then(async (result) => {
         await setDoc(doc(db, "usernames", user.displayName), { uid: user.uid });
     }
     window.location.href = "./predictions.html";
-
-    }).catch((error) => {
-        console.error(error.code, error.message);
-    });
+});
 
 const googleLogin = document.getElementById("google-button");
 googleLogin.addEventListener("click", function() {
@@ -28,16 +22,7 @@ googleLogin.addEventListener("click", function() {
             console.error(error.code, error.message);
         });
     } else {
-        signInWithPopup(auth, provider).then(async (result) => {
-            const user = result.user;
-            const userRef = doc(db, "predicciones", user.uid);
-            const userSnap = await getDoc(userRef);
-            if (!userSnap.exists()) {
-                await setDoc(userRef, { name: user.displayName, email: user.email }, { merge: true });
-                await setDoc(doc(db, "usernames", user.displayName), { uid: user.uid });
-            }
-            window.location.href = "./predictions.html";
-        }).catch((error) => {
+        signInWithPopup(auth, provider).catch((error) => {
             console.error(error.code, error.message);
         });
     }
