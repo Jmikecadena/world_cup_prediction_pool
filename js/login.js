9b0647d8f362561616ce4771cdd3ced7718719ec
Mike <jcadena0731@gmail.com>
2026-04-30 03:08:25 -0500

    update on google auth login for mobile devices using signInWithRedirect

--- js/login.js
+++ js/login.js
@@ -1,21 +1,34 @@
 import { auth, provider, db } from "./firebase.js";
-import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { signInWithPopup, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+async function handleUser(user) {
+    const userRef = doc(db, "predicciones", user.uid);
+    await setDoc(userRef, {
+        name: user.displayName,
+        email: user.email
+    }, { merge: true });
+    window.location.href = "./predictions.html";
+}
+
+getRedirectResult(auth).then(async (result) => {
+    if (result) await handleUser(result.user);
+}).catch((error) => {
+    console.error(error.code, error.message);
+});
+
+const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
 
 const googleLogin = document.getElementById("google-button");
 googleLogin.addEventListener("click", function() {
-    signInWithPopup(auth, provider)
-    .then(async (result) => {
-        const user = result.user;
-        const userRef = doc(db, "predicciones", user.uid);
-        await setDoc(userRef, {
-            name: user.displayName,
-            email: user.email
-        }, { merge: true });
-        window.location.href = "./predictions.html";
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(errorCode, errorMessage);
-    });
+    if (isMobile) {
+        signInWithRedirect(auth, provider);
+    } else {
+        signInWithPopup(auth, provider)
+        .then(async (result) => {
+            await handleUser(result.user);
+        }).catch((error) => {
+            console.error(error.code, error.message);
+        });
+    }
 });
