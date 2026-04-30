7c3dcc6e23e466d8dd1ac562d49f4a8b117b803e
Mike <jcadena0731@gmail.com>
2026-04-30 02:19:42 -0500

    auth changes

--- js/login.js
+++ js/login.js
@@ -1,11 +1,14 @@
 import { auth, provider, db } from "./firebase.js";
-import { signInWithPopup, signInWithRedirect, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { signInWithPopup, signInWithRedirect, getRedirectResult, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+let iniciado = false;
 
-onAuthStateChanged(auth, async (user) => {
-    if (!user) return;
+async function saveUserAndRedirect(user) {
+    if (iniciado) return;
+    iniciado = true;
+
     const userRef = doc(db, "predicciones", user.uid);
     const userSnap = await getDoc(userRef);
     if (!userSnap.exists()) {
@@ -13,6 +16,22 @@ onAuthStateChanged(auth, async (user) => {
         await setDoc(doc(db, "usernames", user.displayName), { uid: user.uid });
     }
     window.location.href = "./predictions.html";
+}
+
+if (isMobile) {
+    getRedirectResult(auth)
+        .then((result) => {
+            if (result && result.user) {
+                saveUserAndRedirect(result.user);
+            }
+        })
+        .catch((error) => {
+            console.error(error.code, error.message);
+        });
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) saveUserAndRedirect(user);
 });
 
 const googleLogin = document.getElementById("google-button");
