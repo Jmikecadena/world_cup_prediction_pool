3869cb25b8999372c012a18b8454d269f606c01c
Mike <jcadena0731@gmail.com>
2026-04-29 19:53:46 -0500

    mobile auth correction

--- js/login.js
+++ js/login.js
@@ -24,7 +24,18 @@ getRedirectResult(auth).then(async (result) => {
 const googleLogin = document.getElementById("google-button");
 googleLogin.addEventListener("click", function() {
     if (isMobile) {
-        signInWithRedirect(auth, provider);
+        signInWithRedirect(auth, provider).then(async (result) => {
+            const user = result.user;
+            const userRef = doc(db, "predicciones", user.uid);
+            const userSnap = await getDoc(userRef);
+            if (!userSnap.exists()) {
+                await setDoc(userRef, { name: user.displayName }, { merge: true });
+                await setDoc(doc(db, "usernames", user.displayName), { uid: user.uid });
+            }
+            window.location.href = "./predictions.html";
+        }).catch((error) => {
+            console.error(error.code, error.message);
+        });
     } else {
         signInWithPopup(auth, provider).then(async (result) => {
             const user = result.user;
